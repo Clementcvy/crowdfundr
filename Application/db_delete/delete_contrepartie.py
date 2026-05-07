317d66b3afe4005f44e4a6abbd0ebb9955d1a78c
Clement Chauvy <clement.chauvy@etu.utc.fr>
2026-05-07 09:38:51 +0000

    ajout delete_contrepartie et contribution

---
+++ Application/db_delete/delete_contrepartie.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import psycopg2
+
+def delete_contrepartie(conn):
+    cur = conn.cursor()
+
+    num = input("Entrez l'id de la contrepartie à supprimer : ")
+
+    sql = "DELETE FROM Contrepartie_numerique WHERE id_c='%s'" % (num)
+    try :
+        cur.execute(sql)
+    except psycopg2.IntegrityError as e:
+        print("Message système :", e)
+    sql = "DELETE FROM Contrepartie_physique WHERE id_c='%s'" % (num)
+    try :
+        cur.execute(sql)
+    except psycopg2.IntegrityError as e:
+        print("Message système :", e)
+    sql = "DELETE FROM Contrepartie WHERE id_c='%s'" % (num)
+    try :
+        cur.execute(sql)
+    except psycopg2.IntegrityError as e:
+        print("Message système :", e)
+    conn.commit()
+
---
+++ Application/db_delete/delete_contribution.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import psycopg2
+
+def delete_contribution(conn):
+    cur = conn.cursor()
+
+    num = input("Entrez l'id de la contribution à supprimer : ")
+
+    sql = "DELETE FROM Contribution WHERE id='%s'" % (num)
+    try :
+        cur.execute(sql)
+    except psycopg2.IntegrityError as e:
+        print("Message système :", e)
+    conn.commit()
+
