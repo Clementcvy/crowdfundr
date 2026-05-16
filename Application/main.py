32fa5d71c7c534ac80f4ff079cbfac94a55dda95
clementcvy <clement.nicolas.chauvy@gmail.com>
2026-05-16 08:44:08 +0200

    delete contrepartie

--- Application/main.py
+++ Application/main.py
@@ -100,7 +100,7 @@ def handleMenu(rep, id, conn):
                     case 2:
                         insert_contrepartie(conn, id)
                     case 3:
-                        delete_contrepartie(conn)
+                        delete_contrepartie(conn, id)
                     case _:
                         print("Choix inconnu.")
                         time.sleep(0.5)
