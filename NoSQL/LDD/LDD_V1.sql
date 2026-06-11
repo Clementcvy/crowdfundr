2681c49ca6d04d91db29918dd364738388539c3c
Tom Bonhomme <tom.bonhomme@etu.utc.fr>
2026-06-11 11:28:45 +0200

    no sql prefix

--- NoSQL/LDD/LDD_V1.sql
+++ NoSQL/LDD/LDD_V1.sql
@@ -1,94 +1,52 @@
-CREATE TABLE Incubateur (
+CREATE SCHEMA nosql;
 
+CREATE TABLE nosql.Incubateur (
     nom VARCHAR(20) PRIMARY KEY,
-
     creation INT NOT NULL, -- Représente l'année (int[4])
-
     budget FLOAT NOT NULL,
-
-    CONSTRAINT annee CHECK (creation BETWEEN 1 AND 9999)
-
+CONSTRAINT annee CHECK (creation BETWEEN 1 AND 9999)
 );
 
-CREATE TYPE type_projet as ENUM('projet_techno', 'projet_social', 'projet_artis');
+CREATE TYPE nosql.typeProjet AS ENUM('projet_techno', 'projet_social', 'projet_artis');
 
-
-CREATE TABLE Projet (
-
+CREATE TABLE nosql.Projet (
     id INT PRIMARY KEY,
-
     titre VARCHAR(20) NOT NULL,
-
     descr TEXT NOT NULL,
-
     objectif FLOAT NOT NULL,
-
     lancement DATE NOT NULL,
-
     incubateur VARCHAR(20),
+    type_projet nosql.typeProjet NOT NULL,
+    membre JSON NOT NULL,
+    avis JSON NOT NULL,
+CONSTRAINT projet_incubateur FOREIGN KEY (incubateur) REFERENCES nosql.Incubateur(nom)
+);
 
-    type_projet 
-
-    membre   JSON NOT NULL 
-
-    avis JSON NOT NULL
-
-    CONSTRAINT projet_incubateur FOREIGN KEY (incubateur) REFERENCES Incubateur(nom)
-
-);   
-
-
-CREATE TABLE Contribution (
-
+CREATE TABLE nosql.Contribution (
     id INT PRIMARY KEY,
-
     date_c TIMESTAMP NOT NULL,
-
     montant FLOAT NOT NULL,
-
     projet INT NOT NULL,
-
     contributeur JSON NOT NULL,
-
-    CONSTRAINT contrib_projet FOREIGN KEY (projet) REFERENCES Projet(id)
-
+CONSTRAINT contrib_projet FOREIGN KEY (projet) REFERENCES nosql.Projet(id)
 );
 
-
-CREATE TABLE Contrepartie (
-
+CREATE TABLE nosql.Contrepartie (
     id_c INT PRIMARY KEY,
-
-    CONSTRAINT contrib FOREIGN KEY (id_c) REFERENCES Contribution(id)
-
+CONSTRAINT contrib FOREIGN KEY (id_c) REFERENCES nosql.Contribution(id)
 );
 
-
-CREATE TABLE Contrepartie_physique (
-
+CREATE TABLE nosql.Contrepartie_physique (
     id_c INT PRIMARY KEY,
-
     poids FLOAT NOT NULL,
-
     frais FLOAT NOT NULL,
-
     transporteur JSON NOT NULL,
-
-    
-
-    CONSTRAINT contrepartie FOREIGN KEY (id_c) REFERENCES Contrepartie(id_c)
-
+CONSTRAINT contrepartie FOREIGN KEY (id_c) REFERENCES nosql.Contrepartie(id_c)
 );
 
-
-CREATE TABLE Contrepartie_numerique (
-
+CREATE TABLE nosql.Contrepartie_numerique (
     id_c INT PRIMARY KEY,
-
     format VARCHAR(10) NOT NULL,
-
     taille INT NOT NULL,
-
-    CONSTRAINT contrepartie FOREIGN KEY (id_c) REFERENCES Contrepartie(id_c)
-
+CONSTRAINT contrepartie FOREIGN KEY (id_c) REFERENCES nosql.Contrepartie(id_c)
 );
